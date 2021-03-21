66600fb5662b230c9db1e07552770f7db1f7bbcd
root <root@localhost.localdomain>
2021-03-21 23:51:04 +0530

    dep

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'mongodb://fun:iWkhr5WdlFZypAgjIVlnObFcVaiWEEsRAlK2mV0O9DrKHCBvaDx6Iv4Sz8wjmHa8fFed30uWLQ8nc8Je4TVSIg==@fun.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@fun@/';
+const url = 'https://fun.mongo.cosmos.azure.com:10255/test';
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
 export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
